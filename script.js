15613abeb2fcd5cc6939b2691becbe35d09ab6bb
AkashChauhan22110 <akashchauhan22110@gmail.com>
2023-08-18 19:21:24 +0530

    done

---
+++ script.js
@@ -0,0 +1,15 @@
+gsap.to("#nav", {
+  backgroundColor: "#000",
+  duration: 0.5,
+  height: "110px",
+  scrollTrigger: {
+    trigger: "#nav",
+    scroller: "body",
+    // markers:true,
+    start: "top -10%",
+    end: "top -11%",
+    scrub: 1,
+  },
+});
+
+ 
